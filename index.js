50053428275cbbe8b8038b0b9258e93a21097a10
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-06-11 16:46:16 +0600

    .env

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 require('dotenv').config()
 const cors = require('cors')
 const jwt = require('jsonwebtoken');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
@@ -263,7 +264,20 @@ app.put('/user', async (req, res) => {
     res.send(result)
   })
 
+  // payment intent
+  app.post("/create-payment-intent", async (req, res) =>{
+    const {price} = req.body
+    const amount = parseInt(price * 100)
 
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: "usd",
+      payment_method_types: ['card']
+    })
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  })
 
 
 
